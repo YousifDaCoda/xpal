d4a1d650e2177d1fba3ff80b9235b204085bffce
rtpini <rtpini18@vt.edu>
2025-04-28 22:42:19 -0400

    Created Supporting Files

---
+++ XPal/Supporting Files/Globals.swift
@@ -0,0 +1,26 @@
+//
+//  Globals.swift
+//  PhotosVideos
+//
+//  Created by Ryan Pini & Osman Balci on 4/1/25.
+//  Copyright © 2025 Osman Balci. All rights reserved.
+//
+//-----------------------------------------
+// Global Alert Title and Message Variables
+//-----------------------------------------
+var alertTitle = ""
+var alertMessage = ""
+
+//------------------------
+// My YouTube Data API Key
+//------------------------
+
+let myApiKey = "ADD_KEY_HERE"
+
+
+//------------------------------
+// YouTube Data API HTTP Headers
+//------------------------------
+let youTubeDataApiHeaders = [
+    "ADD HEADERS HERE"
+]
