28784254fc453ac77b2f4e1a1aca1b0a29c5b503
Gunnar Montes <gunnarmontes@vt.edu>
2025-05-07 22:10:25 -0400

    Home Page and authentication

---
+++ XPal/Startup Views/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  XPal
+//
+//  Created by Yousif Abuhaija on 4/24/25.
+//
+
+import SwiftUI
+
+struct EmptyView: View {
+    var body: some View {
+        Text("Empty View")
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            
+            Tab("Home", systemImage: "house") {
+                EmptyView()
+            }
+            
+            Tab("Trips", systemImage: "list.bullet") {
+                TripsList()
+            }
+            
+            Tab("Translate", systemImage: "mappin.and.ellipse") {
+                Translate()
+            }
+            
+            Tab("Languages", systemImage: "magnifyingglass") {
+                LanguagesList()
+            }
+            
+            Tab("Search API", systemImage: "cloud.sun.rain") {
+                EmptyView()
+            }
+            
+            Tab("Search DB", systemImage: "cloud.sun.rain") {
+                EmptyView()
+            }
+            
+            Tab("Photos", systemImage: "cloud.sun.rain") {
+                EmptyView()
+            }
+            
+            Tab("Weather", systemImage: "cloud.sun.rain") {
+                EmptyView()
+            }
+            
+            Tab("Puzzle", systemImage: "cloud.sun.rain") {
+                EmptyView()
+            }
+            
+            Tab("Settings", systemImage: "gear") {
+                Settings()
+            }
+        }
+        .tabViewStyle(.sidebarAdaptable)
+    }
+}
